bda4e0fc86ed7b6e254a8ec163a61ec22c13b7fb
Jacky <jlian5@ocdsb.ca>
2018-03-28 10:01:43 -0400

    Done #1

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 # Assignment 1
 # ICS3U
-# <your name>
+# Jacky Liang
 # March 28, 2018
 
-###### uncomment this when you are ready to work on it
-#def CtoF ():
-#
+def CtoF (C):
+        '''Converts temperature in Celsius to Fahrenheit'''
+        F = (1.8)*C+32
+        return round(F)
 
 ###### uncomment this when you are ready to work on it
 #def FtoC ():
 #
 
-temperature = int(input('Enter your temperature in Celsius: '))
-print(temperature)
+temperature = float(input('Enter your temperature in Celsius: '))
+print(CtoF(temperature), "Fahrenheit")
