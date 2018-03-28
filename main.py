b48ae3a0af7598d0bc4da24772562266bd7790a0
Jacky <jlian5@ocdsb.ca>
2018-03-28 10:58:37 -0400

    Done #3

--- main.py
+++ main.py
@@ -14,11 +14,21 @@ def FtoC (F):
         return round(C)
 
 
+#Ask for what conversion
 Question = input("What conversion do you want (C = In celsius, F = In Fahrenheit)? " )
 
+#Contiunes program depending on what was answered
 if Question == "F" :
-        temperature = float(input('Enter your temperature in Celsius: '))
-        print(CtoF(temperature), "Fahrenheit")
+        temperature = float(input('Enter your temperature in Celsius: ')) #asks for number
+        if temperature >= -273.15 : #check if input is too low
+                print(CtoF(temperature), "Fahrenheit") #if passed does the conversion
+        else :
+                print("invalid input") #gives msg
+                
 elif Question == "C" :
         temperature = float(input('Enter your temperature in Fahrenheit: '))
-        print(FtoC(temperature), "Celsius") 
+        if temperature >= -459.67 :
+                print(FtoC(temperature), "Celsius")
+        else :
+                print("invalid input")
+
