7ca537a4f6c77c4ba7523ff228037139c948802a
Jacky <jlian5@ocdsb.ca>
2018-03-29 09:41:41 -0400

    Done #5

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Assignment 1
+# Assignment 1 (The Tempverter!)
 # ICS3U
 # Jacky Liang
 # March 28, 2018
@@ -17,26 +17,43 @@ def FtoC (F):
 
 #The Program
 
-Run = True #Bool to check if want to continue running program
+Run = True #Bool to run program
+Another = "Y" #Bool to check if want to do another run
+Counter = 0 #Check how many times program is used
 
-#Ask for what conversion
-Question = input("What conversion do you want (C = In celsius, F = In Fahrenheit)? " )
 
 #Loop to let the program run multi times
 while Run == True :
-        #Contiunes program depending on what was answered
-        if Question == "F" and Run == True :
-                temperature = float(input('Enter your temperature in Celsius: ')) #asks for number
-                if temperature >= -273.15 : #Check if input is too low
-                        print(CtoF(temperature), "Fahrenheit") #If passed does the conversion
-                        Run = False #Ends the run
+        if Another == "N" : #When doesn't want do another
+                print("Thanks for using this program!") #End msg
+                print("You used it", Counter , "time(s)!") #tells how many times used
+                Run = False # end program
+        elif Another == "Y":
+                if Counter == 0 : #First run msg
+                        print("Welcome to the Tempverter! (Temperature Converter)")
+                        print("*THIS PROGRAM IS CASE SENSITIVE*") #warning msg                       
+
+                Question = input("What conversion do you want (C = In celsius, F = In Fahrenheit)? " ) #Ask for what conversion
+                #Contiunes program depending on what was answered
+                if Question == "F" :
+                        temperature = float(input('Enter your temperature in Celsius: ')) #asks for number
+                        if temperature >= -273.15 : #Check if input is too low
+                                Counter += 1 #Counts how many times program ran
+                                print(CtoF(temperature), "Fahrenheit") #If passed does the conversion
+                                Another = input("Another conversion (Y = yes, N = no)? ")  #Ask for another conversion                               
+                        else :
+                                print("Sorry, input too low.") #Gives error msg
+                elif Question == "C":
+                        temperature = float(input('Enter your temperature in Fahrenheit: '))
+                        if temperature >= -459.67 :
+                                Counter += 1
+                                print(FtoC(temperature), "Celsius")
+                                Another = input("Another conversion (Y = yes, N = no)? ")
+                        else :
+                                print("Sorry, input too low.")
                 else :
-                        print("invalid input") #Gives msg
-        elif Question == "C" :
-                temperature = float(input('Enter your temperature in Fahrenheit: '))
-                if temperature >= -459.67 :
-                        print(FtoC(temperature), "Celsius")
-                        Run = False #Ends the run
-                else :
-                        print("invalid input")
+                        print("Sorry, I couldn't understand that.")
+        else :
+                print("Sorry, I couldn't understand that.")
+                Another = input("Another conversion (Y = yes, N = no)? ")
 
