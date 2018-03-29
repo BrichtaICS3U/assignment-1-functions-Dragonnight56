cc7c88cf478f087d7817836fe930627aaaaf4db7
Jacky <jlian5@ocdsb.ca>
2018-03-29 08:51:58 -0400

    Done #4

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Jacky Liang
 # March 28, 2018
 
+#Coversion funcs
 def CtoF (C):
         '''Converts temperature in Celsius to Fahrenheit'''
         F = (1.8)*C+32
@@ -14,21 +15,28 @@ def FtoC (F):
         return round(C)
 
 
+#The Program
+
+Run = True #Bool to check if want to continue running program
+
 #Ask for what conversion
 Question = input("What conversion do you want (C = In celsius, F = In Fahrenheit)? " )
 
-#Contiunes program depending on what was answered
-if Question == "F" :
-        temperature = float(input('Enter your temperature in Celsius: ')) #asks for number
-        if temperature >= -273.15 : #check if input is too low
-                print(CtoF(temperature), "Fahrenheit") #if passed does the conversion
-        else :
-                print("invalid input") #gives msg
-                
-elif Question == "C" :
-        temperature = float(input('Enter your temperature in Fahrenheit: '))
-        if temperature >= -459.67 :
-                print(FtoC(temperature), "Celsius")
-        else :
-                print("invalid input")
+#Loop to let the program run multi times
+while Run == True :
+        #Contiunes program depending on what was answered
+        if Question == "F" and Run == True :
+                temperature = float(input('Enter your temperature in Celsius: ')) #asks for number
+                if temperature >= -273.15 : #Check if input is too low
+                        print(CtoF(temperature), "Fahrenheit") #If passed does the conversion
+                        Run = False #Ends the run
+                else :
+                        print("invalid input") #Gives msg
+        elif Question == "C" :
+                temperature = float(input('Enter your temperature in Fahrenheit: '))
+                if temperature >= -459.67 :
+                        print(FtoC(temperature), "Celsius")
+                        Run = False #Ends the run
+                else :
+                        print("invalid input")
 
